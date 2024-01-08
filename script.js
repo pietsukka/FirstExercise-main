933370b708b9309f243266ead21724d32a6089a1
Juho-Ville <ejuui001@edu.xamk.fi>
2024-01-08 11:59:37 +0200

    Finnished the task

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+
+
+
+// Action Button
+
 document.getElementById("actionButton").addEventListener("click", function() {
+   
+// Calling the toggle effect
+    ToggleDescription();
+
     alert("Button clicked!");
 });
+
+// Datetime
+document.getElementById("currentYear").innerText = new Date().getFullYear();
+
+
+
+ // Toggle function 
+
+function ToggleDescription() {
+    var paragraph = document.getElementById("description");
+    paragraph.style.display = (paragraph.style.display === "none" || paragraph.style.display === "") ? "block" : "none";
+}
